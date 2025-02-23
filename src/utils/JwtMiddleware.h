f7b7afe021a6758e804fe54d5f1e05c0d8349e84
omini <suchmiracle@gmail.com>
2025-02-23 18:41:34 +0300

    update middleware

--- src/utils/JwtMiddleware.h
+++ src/utils/JwtMiddleware.h
@@ -9,6 +9,7 @@ public:
     void doFilter(const drogon::HttpRequestPtr &req, 
                     drogon::FilterCallback &&fcb, drogon::FilterChainCallback &&fccb) override {
         auto authHeader = req->getHeader("Authorization");
+        auto config = drogon::app().getCustomConfig();
 
         if (authHeader.empty() || authHeader.find("Bearer ") != 0) {
             auto res = drogon::HttpResponse::newHttpResponse();
@@ -17,10 +18,25 @@ public:
             fcb(res);
             return;
         }
+        else if (!config.isMember("jwt") || !config["jwt"].isMember("issuer") || !config["jwt"].isMember("secret"))
+        {
+            auto res = drogon::HttpResponse::newHttpResponse();
+            res->setStatusCode(drogon::k500InternalServerError);
+            res->setBody("Server configuration error: Missing JWT settings");
+            fcb(res);
+            return;
+        }
 
-        std::string token = authHeader.substr(7);
+        std::string secretKey = config["jwt"]["secret"].asString();
+        std::string issuer = config["jwt"]["issuer"].asString();
+        std::string tokenWithoutBearer = authHeader.substr(7);
         try {
-            auto decoded = jwt::decode(token);
+            auto decoded = jwt::decode(tokenWithoutBearer);
+            auto verifier = jwt::verify()
+                                .allow_algorithm(jwt::algorithm::hs256{secretKey})
+                                .with_issuer(issuer);
+
+            verifier.verify(decoded);
             req->attributes()->insert("user_id", decoded.get_payload_claim("user_id").as_string());
             fccb();
         } catch (const std::exception &e) {
